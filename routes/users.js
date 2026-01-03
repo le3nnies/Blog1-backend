3ddadc1847750cbe9e8ebae5c1c891f4759e7c10
le3nnies <lennywamuyu055@gmail.com>
2026-01-03 13:49:01 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { authMiddleware, adminMiddleware } = require('../middleware/auth');
+//const { authMiddleware, adminMiddleware } = require('../middleware/auth');
 
 // All routes require authentication and admin privileges
 router.use(authMiddleware);
